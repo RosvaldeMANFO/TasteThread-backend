336091516d2d5415407947f0e57b46ba4fcbdfad
Rosvadle MANFO <florientmanfo3@outlook.fr>
2025-07-21 03:39:51 +0200

    Feat: Debug for sql connection name

--- src/main/kotlin/com/florientmanfo/api/module/Database.kt
+++ src/main/kotlin/com/florientmanfo/api/module/Database.kt
@@ -14,9 +14,16 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.configureDatabase() {
     val config = this.environment.config
+    val connectionName = config.property("ktor.database.connectionName").getString()
+    val completeUrl  = if(!connectionName.isBlank()){
+        println("Using Cloud SQL connection name $connectionName")
+        config.property("ktor.database.url").getString().replace("CLOUD_SQL_CONNECTION_NAME",connectionName)
+    } else {
+        config.property("ktor.database.url").getString()
+    }
     val dbConfig = HikariConfig().apply {
         driverClassName = config.property("ktor.database.driver").getString()
-        jdbcUrl = config.property("ktor.database.url").getString()
+        jdbcUrl = completeUrl
         username = config.property("ktor.database.user").getString()
         password = config.property("ktor.database.password").getString()
         maximumPoolSize = config.property("ktor.database.maximumPoolSize").getString().toInt()
